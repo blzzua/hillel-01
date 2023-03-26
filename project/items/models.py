d32fa7f4d7bfca2292052d2511bff0369fd6c7cb
blzzua <blzzua@github.com>
2023-03-26 14:30:02 +0300

    models Discount

--- project/items/models.py
+++ project/items/models.py
@@ -20,3 +20,26 @@ class Item(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class  (models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(null=True, upload_to='images/items/{id}/image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Discount(models.Model):
+    class DiscountType(models.IntegerChoices):
+        PCT = 0, 'percentage'
+        ABS = 1, 'absolute'
+
+    code = models.CharField(max_length=30),
+    description = models.TextField(blank=True, null=True)
+    discount_type = models.PositiveSmallIntegerField(choices=DiscountType, default=DiscountType.ABS)
+    amount = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
