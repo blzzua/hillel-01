53e7bc80f980e3fff085caeb31da424b38d435ee
blzzua <blzzua@github.com>
2023-03-30 17:52:23 +0300

    upload images, show Category in admin, show Items in admin list only

--- project/project/settings.py
+++ project/project/settings.py
@@ -116,6 +116,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -129,3 +133,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
